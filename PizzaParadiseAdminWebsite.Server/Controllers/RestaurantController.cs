ee766496c565778cecf1621f61e8c2af66891f05
Ben Emmett <bcemmett@users.noreply.github.com>
2023-11-05 21:08:09 +0000

    Make hot reload possible

--- PizzaParadiseAdminWebsite.Server/Controllers/RestaurantController.cs
+++ PizzaParadiseAdminWebsite.Server/Controllers/RestaurantController.cs
@@ -7,14 +7,6 @@ namespace PizzaParadiseAdminWebsite.Server.Controllers
     [Route("api/[controller]")]
     public class RestaurantController : ControllerBase
     {
-        private static readonly Restaurant[] Restaurants = new[]
-        {
-            new Restaurant { id = 1, name = "Restaurant 1", phone = "0123456789"},
-            new Restaurant { id = 2, name = "Restaurant 2", phone = "3456789012"},
-            new Restaurant { id = 3, name = "Restaurant 3", phone = "6789012345"},
-            new Restaurant { id = 4, name = "Restaurant 4", phone = "987654321"},
-        };
-
         private readonly ILogger<RestaurantController> _logger;
 
         public RestaurantController(ILogger<RestaurantController> logger)
@@ -26,7 +18,16 @@ namespace PizzaParadiseAdminWebsite.Server.Controllers
         [Route("list-restaurants")]
         public IEnumerable<Restaurant> Get()
         {
-            return Restaurants;
+            Restaurant[] restaurants = new[]
+            {
+                new Restaurant { id = 1, name = "Restaurant 1", phone = "0123456789"},
+                new Restaurant { id = 2, name = "Restaurant 2", phone = "3456789012"},
+                new Restaurant { id = 3, name = "Restaurant 3", phone = "6789012345"},
+                new Restaurant { id = 4, name = "Restaurant 4", phone = "987654321"},
+                new Restaurant { id = 5, name = "Restaurant 5", phone = "185963"},
+                new Restaurant { id = 6, name = "Restaurant 5", phone = "654987"},
+            };
+            return restaurants;
         }
     }
 }
