d489e8d250c28de3805000f2c1c3720fac5595d6
Ben Emmett <bcemmett@users.noreply.github.com>
2023-11-06 20:51:51 +0000

    Properties should be nullable

--- PizzaParadiseAdminWebsite.Server/Models/Restaurant.cs
+++ PizzaParadiseAdminWebsite.Server/Models/Restaurant.cs
@@ -3,7 +3,7 @@ namespace PizzaParadiseAdminWebsite.Server.Models
     public class Restaurant
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 }
