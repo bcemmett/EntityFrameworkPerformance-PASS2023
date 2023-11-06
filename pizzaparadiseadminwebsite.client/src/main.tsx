422c27f4acd3e42edc4e7293deb687657210d667
Ben Emmett <bcemmett@users.noreply.github.com>
2023-11-06 20:51:44 +0000

    Don't use react strict mode
    Since re-mounting components effectively forces api calls to be made twice

--- pizzaparadiseadminwebsite.client/src/main.tsx
+++ pizzaparadiseadminwebsite.client/src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TabLayout from './components/TabLayout.tsx'
 import HeaderBar from './components/HeaderBar.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
     <HeaderBar />
     <TabLayout />
-  </React.StrictMode>,
+  </>,
 )
